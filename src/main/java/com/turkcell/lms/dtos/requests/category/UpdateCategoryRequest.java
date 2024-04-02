501d431de2ca1ebdd103390d1d4e955345d6e80d
duygu2 <duyguorhan017@gmail.com>
2024-04-02 15:51:55 +0300

    category mapper added

---
+++ src/main/java/com/turkcell/lms/dtos/requests/category/UpdateCategoryRequest.java
@@ -0,0 +1,14 @@
+package com.turkcell.lms.dtos.requests.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateCategoryRequest {
+    private String name;
+}
---
+++ src/main/java/com/turkcell/lms/dtos/responses/category/GetByIdCategoryResponse.java
@@ -0,0 +1,15 @@
+package com.turkcell.lms.dtos.responses.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetByIdCategoryResponse {
+    private int id;
+    private String name;
+}
